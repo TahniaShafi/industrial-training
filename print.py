6191b4572ee1608224bf7e9e0b927c7a3164f42f
Tahnia Binte Shafi <tahniabinteshafi@Tahnias-Laptop.local>
2025-01-29 12:54:46 +0600

    edit print file

--- print.py
+++ print.py
@@ -1,2 +1,10 @@
-for i in range (10):
-    print(i+1)
+def nth_fibonacci(x):
+    
+    if x<=1:
+        return x
+    
+    return nth_fibonacci(x-1) + nth_fibonacci(x-2)
+
+x = 7
+result = nth_fibonacci(x)
+print (result)
